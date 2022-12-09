7c30c8f03e22f1a1b9c4979466c563935d391830
ngaleuj <ngaleujoedan@gmail.com>
2022-12-09 22:01:11 +0100

    implementing Uiid

--- src/fde/dto/log.interceptor.ts
+++ src/fde/dto/log.interceptor.ts
@@ -1,7 +1,10 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, BadGatewayException } from '@nestjs/common';
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
-import { isContext } from 'vm';
+// import { isContext } from 'vm';
+// import {v4 as uuidv4} from 'uuid';
+
+let 
 
 
 @Injectable()
